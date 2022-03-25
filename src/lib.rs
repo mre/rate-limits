a95b0f14c453afff546e1842b94e925f8107ef8c
Matthias <matthias-endler@gmx.net>
2022-03-25 01:10:55 +0100

    add StackOverflow link

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 //! A crate for parsing HTTP rate limit headers as per the [IETF draft][draft].
 //! Inofficial implementations like the [Github rate limit headers][github] are
 //! also supported on a best effort basis.
+//! 
+//! Other resources:
+//! * https://stackoverflow.com/a/16022625/270334
 //!
 //! [github]: https://docs.github.com/en/rest/overview/resources-in-the-rest-api
 //! [draft]: https://tools.ietf.org/id/draft-polli-ratelimit-headers-00.html
